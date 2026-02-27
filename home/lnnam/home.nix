a213f468802db706bc47338fece5d7b99ce11109
namlnhut <nam.lnhut@gmail.com>
2026-02-27 22:27:24 +0700

    update

--- home/lnnam/home.nix
+++ home/lnnam/home.nix
@@ -21,10 +21,11 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    cowsay
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
